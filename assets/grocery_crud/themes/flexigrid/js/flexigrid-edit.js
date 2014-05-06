109ef801ce73ec09ab429293d7d2f73787402428
Raheel Hsn <Rah1x@users.noreply.github.com>
2014-05-06 20:23:52 +0500

    Allow multiple crud forms

--- assets/grocery_crud/themes/flexigrid/js/flexigrid-edit.js
+++ assets/grocery_crud/themes/flexigrid/js/flexigrid-edit.js
@@ -6,48 +6,52 @@ $(function(){
 		if($(this).hasClass('vsble'))
 		{
 			$(this).removeClass('vsble');
-			$('#main-table-box #crudForm').slideDown("slow");
+			$('#main-table-box .crudForm').slideDown("slow");
 		}
 		else
 		{
 			$(this).addClass('vsble');
-			$('#main-table-box #crudForm').slideUp("slow");
+			$('#main-table-box .crudForm').slideUp("slow");
 		}
 	});
 
-	$('#save-and-go-back-button').click(function(){
+	$('.save-and-go-back-button').click(function(){
 		save_and_close = true;
-
-		$('#crudForm').trigger('submit');
+		//$('.crudForm').trigger('submit');
+        $(this).parent().parent().parent('.crudForm').trigger('submit');
 	});
 
-	$('#crudForm').submit(function(){
+	$('.crudForm').submit(function(){
 		var my_crud_form = $(this);
+
+        var FormLoading = $(this).parent().parent().find('.FormLoading');
+        var report_error = $(this).parent().parent().find('.report-error');
+        var report_success = $(this).parent().parent().find('.report-success');
 
 		$(this).ajaxSubmit({
 			url: validation_url,
 			dataType: 'json',
 			cache: 'false',
 			beforeSend: function(){
-				$("#FormLoading").show();
+				$(FormLoading).show(); //"#FormLoading"
 			},
 			success: function(data){
-				$("#FormLoading").hide();
-				if(data.success)
+				$(FormLoading).hide(); //"#FormLoading"
+                if(data.success)
 				{
-					$('#crudForm').ajaxSubmit({
+					//alert($(this).getAttribute('method'));
+                    $(my_crud_form).ajaxSubmit({ //'#crudForm'
 						dataType: 'text',
 						cache: 'false',
 						beforeSend: function(){
-							$("#FormLoading").show();
+							$(FormLoading).show(); //"#FormLoading"
 						},
 						success: function(result){
-
-							$("#FormLoading").fadeOut("slow");
+							$(FormLoading).fadeOut("slow"); //"#FormLoading"
 							data = $.parseJSON( result );
 							if(data.success)
 							{
-								var data_unique_hash = my_crud_form.closest(".flexigrid").attr("data-unique-hash");
+                                var data_unique_hash = my_crud_form.closest(".flexigrid").attr("data-unique-hash");
 
 								$('.flexigrid[data-unique-hash='+data_unique_hash+']').find('.ajax_refresh_and_loading').trigger('click');
 
@@ -63,15 +67,15 @@ $(function(){
 									return true;
 								}
 
-								form_success_message(data.success_message);
+								form_success_message(data.success_message, my_crud_form);
 							}
 							else
 							{
-								form_error_message(message_update_error);
+								form_error_message(message_update_error, my_crud_form);
 							}
 						},
 						error: function(){
-							form_error_message( message_update_error );
+							form_error_message( message_update_error, my_crud_form);
 						}
 					});
 				}
@@ -80,33 +84,34 @@ $(function(){
 					$('.field_error').each(function(){
 						$(this).removeClass('field_error');
 					});
-					$('#report-error').slideUp('fast');
-					$('#report-error').html(data.error_message);
+					$(report_error).slideUp('fast');
+					$(report_error).html(data.error_message);
 					$.each(data.error_fields, function(index,value){
 						$('input[name='+index+']').addClass('field_error');
 					});
 
-					$('#report-error').slideDown('normal');
-					$('#report-success').slideUp('fast').html('');
+					$(report_error).slideDown('normal');
+					$(report_success).slideUp('fast').html('');
 
 				}
 			},
 			error: function(){
 				alert( message_update_error );
-				$("#FormLoading").hide();
+				$(FormLoading).hide(); //"#FormLoading"
 
 			}
 		});
 		return false;
 	});
 
-	if( $('#cancel-button').closest('.ui-dialog').length === 0 ) {
+	if( $('.cancel-button').closest('.ui-dialog').length === 0 ) {
 
-		$('#cancel-button').click(function(){
-			if( $(this).hasClass('back-to-list') || confirm( message_alert_edit_form ) )
-			{
+		$('.cancel-button').click(function()
+        {
+			//if( $(this).hasClass('back-to-list') || confirm( message_alert_edit_form ) )
+			//{
 				window.location = list_url;
-			}
+			//}
 
 			return false;
 		});
